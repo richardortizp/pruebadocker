47ae8c266aeced3b58614c071629ae917a4ee729
richardortizp <rop.dbz@hotmail.com>
2025-06-10 21:04:37 -0600

    Fix app.js write method

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import { Jimp } from "jimp";
 
 const inputPath = "./input.jpeg";
-const outputPath = "./output.jpeg";
+const outputPath = "./output.jpg";
 
 Jimp.read(inputPath)
   .then(image => {
     return image
       .greyscale()
-      .writeAsync(outputPath)
+      .write(outputPath)
       .then(() => {
         console.log(`Imagen convertida a blanco y negro y guardada como ${outputPath}`);
       });
